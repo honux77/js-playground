0923e872262e0bb7aafb0eb59e116f53bd662d76
DESKTOP-R3ML8O0\k5020 <honux77@gmail.com>
2018-03-22 16:11:51 +0900

    [단어퍼즐] 리팩토링
    지저분한 코드를 조금 정리했습니다.
    그런데 여전히 지저분합니다 ㅜㅜ.

--- 15-puzzle/word.js
+++ 15-puzzle/word.js
@@ -1,80 +1,93 @@
-var word1 = document.getElementById('word1');
-var word2 = document.getElementById('word2');
-var check = document.getElementById('check');
+//html elements
+var word1 = document.getElementById('word1'); //answer
+var word2 = document.getElementById('word2'); //buttons
+var check = document.getElementById('check'); //word1 === word2?
 
-var words = 'apple,linux,javascript,tutorial,codesquad,baby,girlfriend,legend'.split(',');
+//game objects
+var game = {
+    'btns': []
+};
 
+game.words = 'apple,linux,javascript,tutorial,codesquad,baby,girlfriend,legend'.split(',');
 
-var game = {};
-game.choice = function() {
-    var idx = Math.floor(Math.random() * words.length);
-    return words[idx];
-}
-var answer = game.choice();
-word1.innerHTML = answer;
+//choose 1 word from words;
+game.choose = function () {
+    var idx = Math.floor(Math.random() * this.words.length);
+    this.answer = this.words[idx];
+    this.letters = this.answer.split('');
+    word1.innerHTML = this.answer;
+};
 
-game.word = answer.split('');
-game.btns = [];
+game.addButtons = function () {
+    for (var i = 0; i < this.letters.length; i++) {
+        var btn = document.createElement('button');
+        btn.innerHTML = this.letters[i];
+        word2.appendChild(btn);
+        this.btns.push(btn);
+    }
+};
 
 game.updateDisplay = function () {
-    if (answer === game.word.join('')) {
+    var gameStr = this.letters.join('');
+    if (this.answer === gameStr) {
         check.innerHTML = '일치합니다';
     } else {
         check.innerHTML = '일치하지 않습니다';
     }
 };
 
-for (var i = 0; i < answer.length; i++) {
-    var btn = document.createElement('button');
-    btn.innerHTML = answer[i];
-    word2.appendChild(btn);
-    game.btns.push(btn);
-}
+game.init = function () {
+    this.choose();
+    this.addButtons();
+    this.updateDisplay();
+};
+game.init();
+
 
 game.copyBtnText = function () {
-    for (var i = 0; i < this.word.length; i++) {
-        this.btns[i].innerHTML = this.word[i];
+    for (var i = 0; i < this.letters.length; i++) {
+        this.btns[i].innerHTML = this.letters[i];
     }
-}
+};
 
-var swap = function (event) {
+//event hadler for swap button
+var swap = function () {
     var temp = [];
     //copy and swap
-    while (game.word.length != 0) {
-        var s = game.word.pop();
+    while (game.letters.length != 0) {
+        var s = game.letters.pop();
         temp.push(s);
     }
 
-    game.word = temp;
+    game.letters = temp;
     game.copyBtnText();
     game.updateDisplay();
 };
 
-var rshift = function (event) {
-    var s = game.word.pop();
-    game.word.unshift(s);
+var rshift = function () {
+    var s = game.letters.pop();
+    game.letters.unshift(s);
     game.copyBtnText();
     game.updateDisplay();
 };
 
-var lshift = function (event) {
-    var s = game.word.shift();
-    game.word.push(s);
+var lshift = function () {
+    var s = game.letters.shift();
+    game.letters.push(s);
     game.copyBtnText();
     game.updateDisplay();
 };
-
 
 //shuffle
+game.shuffle = function () {
+    var toggle = Math.floor(Math.random() * 2) === 0;
+    if (toggle) {
+        swap();
+    }
 
-var toggle = Math.floor(Math.random() * 2) === 0;
-
-if (toggle) {
-    swap();
-}
-
-var n = Math.floor(Math.random() * answer.length);
-
-for (var i = 0; i < n; i++) {
-    rshift();
-}
+    var n = Math.floor(Math.random() * game.answer.length);
+    for (var i = 0; i < n; i++) {
+        rshift();
+    }
+};
+game.shuffle();
